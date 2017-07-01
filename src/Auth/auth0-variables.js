bd9d63aaec4d9edc96b0b5fc9870ff3ad2469be5
kerembakir <kerembakir@gmail.com>
2017-07-01 14:56:50 +0200

    Added history.js and Auth

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+    domain: 'kerembakir.eu.auth0.com',
+    clientId: 'MY_AUTH0_CLIENTID',
+    callbackUrl: 'http://localhost:3000/callback' //Callback URL set in the Auth0 dasbhoard.
+}
